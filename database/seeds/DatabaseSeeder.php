0024fd41456cbdbdd4badc020690174526a41c30
joselrodriguezd <joselrodriguezd81@gmail.com>
2019-04-17 15:18:22 -0500

    rutas commin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +9,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTablas([
+            'rol',
+            'permiso'
+        ]);
         // $this->call(UsersTableSeeder::class);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
+    }
+    protected function truncateTablas(array $tablas)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
